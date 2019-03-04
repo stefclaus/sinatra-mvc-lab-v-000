35090f986bba385297f798e04d1f985517cfa560
Stef Claus <stef.claus@gmail.com>
2019-03-04 00:11:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @piglatinized_text = TextAnalyzer.new(params[:user_text])
+    erb :results
+  end
+
 end
